9a82727aff372329189c16f8889bfb2c66cf2c56
recodingyo <recodingyo@gmail.com>
2020-11-11 13:43:05 +0700

    Upload TravelAPP

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# TravelAPP
-fix selesai
+# TiketSayaApp
+Aplikasi pemesanan tiket buatan dari Angga Risky menggunakan Java dan XMl dan Firebase sebagai data center.
---
+++ app/src/main/java/com/digitalsoftware/firstbelajar/tiketsaya/LoginActi.java
@@ -0,0 +1,13 @@
+package com.digitalsoftware.firstbelajar.tiketsaya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
